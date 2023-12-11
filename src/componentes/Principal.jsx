c32f8c5e171cf962888e1be4d955baa7457e249a
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2023-12-11 17:28:43 -0300

    .

---
+++ src/componentes/Principal.jsx
@@ -0,0 +1,7 @@
+import Principal from 'react'
+
+const Principal = () => {
+  return 
+}
+
+export default Principal
---
+++ src/componentesStyle/Wrapper.style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100vh;
+    padding: 10px;
+    background-color: grey;
+    position: relative;
+`
