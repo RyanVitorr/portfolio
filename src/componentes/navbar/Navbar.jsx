0a6e2a5827df01b633a69807633998a549c926a3
Ryan  Vitor <127523720+RyanVitorr@users.noreply.github.com>
2023-12-15 16:27:07 -0300

    .

--- src/componentes/navbar/Navbar.jsx
+++ src/componentes/navbar/Navbar.jsx
@@ -4,29 +4,28 @@ import '../navbar/NavbarStyle.css';
 
 import Principal from '../principal/Principal';
 
-const Navbar = (home, ) => {
+const Navbar = () => {
   const [state, setState] = useState(false);
   const toggle = ()=> {
     setState(!state);
   };
- 
 
   return (
     <nav className="navbar">
-        <div className="div-icone-puxar-nav" onClick={toggle}></div>
+      <div className="div-icone-puxar-nav" onClick={toggle}></div>
        
-        <div className={"div-principal-nav " + (state ? "ativo":"")}>
-          <ul>
-            <li><i className="fa-solid fa-user"></i></li>
+      <div className={"div-principal-nav " + (state ? "ativo":"")}>
+        <ul>
+          <li><i className="fa-solid fa-user"></i></li>
            
-            <li><i className="fa-solid fa-user"></i></li>
+          <li><i className="fa-solid fa-user"></i></li>
             
-            <li><i className="fa-solid fa-user"></i></li>
+          <li><i className="fa-solid fa-user"></i></li>
             
-            <li><i className="fa-solid fa-user"></i></li>
-          </ul>
-        </div>
-      </nav>
+          <li><i className="fa-solid fa-user"></i></li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
